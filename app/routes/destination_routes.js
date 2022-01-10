dfda409d8786ba7b59c02e1b8d2151c71fe77946
anaismveras <verasmanais@gmail.com>
2022-01-10 16:30:18 -0500

    can add a destination to db

--- app/routes/destination_routes.js
+++ app/routes/destination_routes.js
@@ -9,7 +9,7 @@ const bcrypt = require('bcrypt')
 // pull in error types and the logic to handle them and set status codes
 const errors = require('../../lib/custom_errors')
 const axios = require('axios')
-
+const requireToken = passport.authenticate('bearer', { session: false })
 
 const BadParamsError = errors.BadParamsError
 const BadCredentialsError = errors.BadCredentialsError
@@ -21,10 +21,10 @@ const Review = require('../models/review')
 
 const router = express.Router()
 
-
-// API call to get all destinations
-router.get('/:destination', (req, res, next) => {
-    axios.get(`https://api.roadgoat.com/api/v2/destinations${req.params.destination}`, {
+// API call to GET one destination based on city/country name
+router.get('/destinations/:destinationname', (req, res, next) => {
+    const key_value = `?q=${req.params.destinationname}`
+    axios.get(`https://api.roadgoat.com/api/v2/destinations/auto_complete${key_value}`, {
         headers: { 
             'Authorization': `Basic ${process.env.AUTH_KEY}`
           }
@@ -35,5 +35,41 @@ router.get('/:destination', (req, res, next) => {
     })
     .catch(next)
 })
+
+router.get('/destinations', (req, res, next) => {
+    Destination.find({})
+    .then(des => {
+        res.json(des)
+    })
+    .catch(next)
+})
+
+// POST create a destination into database
+router.post('/destinations', (req, res, next) => {
+    Destination.findOne({
+        roadGoatId: req.body.roadGoatId
+    }).populate('reviews')
+    .then(des => {
+        if (des) {
+            des.users.push(req.user._id)
+            return des.save()
+        } else {
+            return Destination.create({
+                    city: req.body.city,
+                    state: req.body.state,
+                    country: req.body.country,
+                    reviews: [],
+                    lgbtRating: req.body.lgbtRating,
+                    image_url: req.body.image_url,
+                    description: req.body.description,
+                    business: [],
+                    roadGoatId: req.body.roadGoatId
+            })
+        }
+    })
+    .then(data => res.status(200).json(data))
+    .catch(next)
+})
+
 
 module.exports = router
