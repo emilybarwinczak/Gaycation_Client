bc6f8e456a3d57cf7261c020f84328191cb6e083
anaismveras <verasmanais@gmail.com>
2022-01-10 10:44:59 -0500

    trying to figure out how to call api from backend

--- app/routes/destination_routes.js
+++ app/routes/destination_routes.js
@@ -11,12 +11,7 @@ const axios = require('axios')
 
 const BadParamsError = errors.BadParamsError
 const BadCredentialsError = errors.BadCredentialsError
-// jsonwebtoken docs: https://github.com/auth0/node-jsonwebtoken
-// Passport docs: http://www.passportjs.org/docs/
-
-// see above for explanation of "salting", 10 rounds is recommended
 const bcryptSaltRounds = 10
-
 
 const Destination = require('../models/destination')
 const User = require('../models/user')
@@ -25,18 +20,23 @@ const Review = require('../models/review')
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
 // it will also set `res.user`
-const requireToken = passport.authenticate('bearer', { session: false })
+// const requireToken = passport.authenticate('bearer', { session: false })
 const API_KEY = process.env.API_KEY
+const SECRET_KEY = process.env.SECRET_KEY
 // instantiate a router (mini app that only handles routes)
 const router = express.Router()
+const auth_key = Buffer.from(`${API_KEY}:${SECRET_KEY}`).toString('base64');
+
 
 // API call to get all destinations
-router.get('/destinations', requireToken, (req, res, next) => {
-    axios.get('https://api.roadgoat.com:80/api/v2/destinations/auto_complete?q=barcelona', {
-        headers: {
-            'Authorization': `Basic ${API_KEY}`
-        }
-    }).then(resp => {
-        console.log('Destinations:\n', resp.data)
-    }).catch(next)
-})
+// router.get('/destinations', (req, res, next) => {
+//     axios.get('https://api.roadgoat.com/api/v2/destinations/auto_complete?q=barcelona', {
+//         headers: {
+//             'Authorization': `Basic ${auth_key}`
+//         }
+//     }).then(resp => {
+//         console.log('Destination:\n', resp.data)
+//     }).catch(next)
+// })
+
+module.exports = router
